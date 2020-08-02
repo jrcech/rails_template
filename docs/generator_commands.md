87d9ee7ae9338ccfb665a2f44da424bded8288c1
Jiří Čech <cech@iMac.local>
2020-08-02 18:37:18 +0200

    Refactor template.rb

--- docs/generator_commands.md
+++ docs/generator_commands.md
@@ -5,7 +5,7 @@
 rails new base -STB --database=postgresql --skip-spring --skip-turbolinks --skip-system-test --webpack=stimulus --skip-webpack-install
 ```
 
-## Generate app fast with template
-```bash
-rails new test1 -m ./rails_template/template.rb -STB --database=postgresql --skip-spring --skip-turbolinks --skip-system-test --webpack=stimulus --skip-webpack-install
+## Generate app fast with template and force overwrite
+```shell
+rails new test1 -m ./rails_template/template.rb -fSTB --database=postgresql --skip-spring --skip-turbolinks --skip-system-test --webpack=stimulus --skip-webpack-install
 ```
--- template.rb
+++ template.rb
@@ -8,14 +8,20 @@ def remove_file_comments(file)
   gsub_file(file, /^\s*#.*$\n/, '')
 end
 
-def remove_comments(files)
+def remove_all_comments(files)
   files.each do |file|
     remove_file_comments file
   end
 end
 
-def remove_whitespaces(file)
+def remove_file_whitespaces(file)
   gsub_file(file, /\S\K([ ]{2,})/, ' ')
+end
+
+def remove_all_whitespaces(files)
+  files.each do |file|
+    remove_file_whitespaces file
+  end
 end
 
 def rubocop_file(file)
@@ -66,65 +72,15 @@ Gemfile
 Rakefile
 ]
 
-remove_comments(commented_files)
+remove_all_comments commented_files
 
-remove_whitespaces 'config/environments/production.rb'
-remove_whitespaces 'config/environments/test.rb'
-remove_whitespaces 'config/puma.rb'
-
-copy_file 'files/.rubocop.yml', '.rubocop.yml'
-
-rubocop_files = %w[
-app/jobs/application_job.rb
-config/environments/development.rb
+multiple_whitespace_files = %w[
 config/environments/production.rb
 config/environments/test.rb
-config/initializers/application_controller_renderer.rb
-config/initializers/backtrace_silencers.rb
-config/initializers/content_security_policy.rb
-config/initializers/cookies_serializer.rb
-config/initializers/filter_parameter_logging.rb
-config/initializers/inflections.rb
-config/initializers/mime_types.rb
-config/initializers/wrap_parameters.rb
-config/application.rb
-config/boot.rb
-config/database.yml
-config/environment.rb
 config/puma.rb
-config/routes.rb
-config.ru
-Gemfile
-Rakefile
 ]
 
+remove_all_whitespaces multiple_whitespace_files
+
+copy_file 'files/.rubocop.yml', '.rubocop.yml'
 rubocop_correct_all
-
-# rubocop_file 'app/jobs/application_job.rb'
-# rubocop_file 'config/environments/development.rb'
-# rubocop_file 'config/environments/production.rb'
-# rubocop_file 'config/environments/test.rb'
-# rubocop_file 'config/initializers/application_controller_renderer.rb'
-# rubocop_file 'config/initializers/backtrace_silencers.rb'
-# rubocop_file 'config/initializers/cookies_serializer.rb'
-# rubocop_file 'config/initializers/content_security_policy.rb'
-# rubocop_file 'config/initializers/filter_parameter_logging.rb'
-# rubocop_file 'config/initializers/inflections.rb'
-# rubocop_file 'config/initializers/mime_types.rb'
-# rubocop_file 'config/initializers/wrap_parameters.rb'
-# rubocop_file 'Gemfile'
-# rubocop_file 'config/application.rb'
-# rubocop_file 'config/rubocop.rb'
-# rubocop_file 'config/rubocop.rb'
-# rubocop_file 'config/puma.rb'
-# rubocop_file 'config/routes.rb'
-# rubocop_file 'Rakefile'
-# rubocop_file 'config.ru'
-
-# Frozen string literals
-# rubocop_file 'app/channels/application_cable/channel.rb'
-# rubocop_file 'app/channels/application_cable/connection.rb'
-# rubocop_file 'app/controllers/application_controller.rb'
-# rubocop_file 'app/helpers/application_helper.rb'
-# rubocop_file 'app/mailers/application_mailer.rb'
-# rubocop_file 'app/models/application_model.rb'
