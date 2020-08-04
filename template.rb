68dacda02e31ac9d281937a073b42a9a87bd274b
Jiří Čech <cech@iMac.local>
2020-08-04 11:40:14 +0200

    Refactor and correct rubocop issues template.rb

--- template.rb
+++ template.rb
@@ -23,13 +23,7 @@ def rubocop_file(file)
 end
 
 def rubocop_correct_all
-  run("rubocop -a")
-end
-
-def rubocop_files(files)
-  files.each do |file|
-    rubocop_file file
-  end
+  run('rubocop -a')
 end
 
 def eslint
@@ -40,43 +34,58 @@ remove_dir 'app/assets'
 remove_file 'db/seeds.rb'
 
 commented_files = %w[
-app/jobs/application_job.rb
-config/environments/development.rb
-config/environments/production.rb
-config/environments/test.rb
-config/initializers/application_controller_renderer.rb
-config/initializers/backtrace_silencers.rb
-config/initializers/content_security_policy.rb
-config/initializers/cookies_serializer.rb
-config/initializers/filter_parameter_logging.rb
-config/initializers/inflections.rb
-config/initializers/mime_types.rb
-config/initializers/wrap_parameters.rb
-config/locales/en.yml
-config/application.rb
-config/boot.rb
-config/database.yml
-config/environment.rb
-config/puma.rb
-config/routes.rb
-config/storage.yml
-.gitignore
-config.ru
-Gemfile
-Rakefile
+  app/jobs/application_job.rb
+  config/environments/development.rb
+  config/environments/production.rb
+  config/environments/test.rb
+  config/initializers/application_controller_renderer.rb
+  config/initializers/backtrace_silencers.rb
+  config/initializers/content_security_policy.rb
+  config/initializers/cookies_serializer.rb
+  config/initializers/filter_parameter_logging.rb
+  config/initializers/inflections.rb
+  config/initializers/mime_types.rb
+  config/initializers/wrap_parameters.rb
+  config/locales/en.yml
+  config/application.rb
+  config/boot.rb
+  config/database.yml
+  config/environment.rb
+  config/puma.rb
+  config/routes.rb
+  config/storage.yml
+  .gitignore
+  config.ru
+  Gemfile
+  Rakefile
 ]
 
 change_files commented_files, :remove_file_comments
 
 multiple_whitespace_files = %w[
-config/environments/production.rb
-config/environments/test.rb
-config/puma.rb
+  config/environments/production.rb
+  config/environments/test.rb
+  config/puma.rb
 ]
 
 change_files multiple_whitespace_files, :remove_file_whitespaces
 
 copy_file 'files/.rubocop.yml', '.rubocop.yml'
+
+gem_group :development do
+  gem 'brakeman'
+  gem 'bundler-audit'
+  gem 'rspec-expectations'
+  gem 'fasterer'
+  gem 'flay'
+  gem 'overcommit'
+  gem 'rails_best_practices'
+  gem 'reek'
+  gem 'rubocop', require: false
+  gem 'rubocop-rspec', require: false
+  gem 'rubycritic', require: false
+  gem 'slim_lint'
+end
 
 after_bundle do
   say 'After Bundle'
