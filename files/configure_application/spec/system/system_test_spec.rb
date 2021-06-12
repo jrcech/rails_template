fa761136d18f8c4c8b01c920a7b0ddf34fd3c43a
Jiří Čech <jiricech94@gmail.com>
2021-06-12 21:34:20 +0200

    Add frontend tests

--- files/configure_application/spec/system/system_test_spec.rb
+++ files/configure_application/spec/system/system_test_spec.rb
@@ -25,4 +25,19 @@ RSpec.describe 'System test', type: :system do
   it 'ensures styles are loaded', js: true do
     expect(page).to have_css 'p.css-test', visible: :hidden
   end
+
+  it 'ensures font awesome is loaded', js: true do
+    expect(page).to have_css 'svg.fa-check', visible: :visible
+  end
+
+  describe 'bootstrap components' do
+    it 'ensures styles are loaded', js: true do
+      expect(page).to have_css 'p.d-none', visible: :hidden
+    end
+
+    it 'ensures javascript is loaded', js: true do
+      find_button('Bootstrap?').hover
+      expect(page).to have_css 'div.tooltip'
+    end
+  end
 end
